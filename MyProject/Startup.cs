852ae5940ada5c8614843839ecd4a95573798fe9
Karim Obaid <kokarim@msn.com>
2017-11-24 11:49:17 -0500

    Test1

--- MyProject/Startup.cs
+++ MyProject/Startup.cs
@@ -27,7 +27,7 @@ namespace MyProject
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello Karim!");
             });
         }
     }
